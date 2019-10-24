54cfa4e023eeb49adb9b4b43a2ae5e450936d33f
Natasha <nsavelenko@gmail.com>
2019-10-24 20:40:19 +0300

    order2

--- Factory_And_store/StoreManagerForm.Designer.cs
+++ Factory_And_store/StoreManagerForm.Designer.cs
@@ -59,24 +59,31 @@
             this.newsPanel = new System.Windows.Forms.Panel();
             this.merchandisePanel = new System.Windows.Forms.Panel();
             this.orderPanel = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.splitter2 = new System.Windows.Forms.Splitter();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.refreshOrderButton = new System.Windows.Forms.Button();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.storeVisitEmployeeDataGridView)).BeginInit();
             this.workVisitPanel.SuspendLayout();
             this.orderPanel.SuspendLayout();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -108,25 +115,25 @@
             // makeAnOrderToolStripMenuItem
             // 
             this.makeAnOrderToolStripMenuItem.Name = "makeAnOrderToolStripMenuItem";
-            this.makeAnOrderToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.makeAnOrderToolStripMenuItem.Size = new System.Drawing.Size(130, 26);
             this.makeAnOrderToolStripMenuItem.Text = "Make";
             this.makeAnOrderToolStripMenuItem.Click += new System.EventHandler(this.makeAnOrderToolStripMenuItem_Click);
             // 
             // acceptToolStripMenuItem
             // 
             this.acceptToolStripMenuItem.Name = "acceptToolStripMenuItem";
-            this.acceptToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.acceptToolStripMenuItem.Size = new System.Drawing.Size(130, 26);
             this.acceptToolStripMenuItem.Text = "Accept";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(213, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(127, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(130, 26);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // merchandiseToolStripMenuItem
@@ -140,7 +147,7 @@
             // updateToolStripMenuItem
             // 
             this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
-            this.updateToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
             this.updateToolStripMenuItem.Text = "Update";
             // 
             // workVisitToolStripMenuItem
@@ -154,7 +161,7 @@
             // setStartTimeToolStripMenuItem
             // 
             this.setStartTimeToolStripMenuItem.Name = "setStartTimeToolStripMenuItem";
-            this.setStartTimeToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.setStartTimeToolStripMenuItem.Size = new System.Drawing.Size(154, 26);
             this.setStartTimeToolStripMenuItem.Text = "Input visits";
             this.setStartTimeToolStripMenuItem.Click += new System.EventHandler(this.setStartTimeToolStripMenuItem_Click);
             // 
@@ -375,9 +382,30 @@
             this.orderPanel.TabIndex = 5;
             this.orderPanel.Visible = false;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 390);
+            this.dataGridView1.TabIndex = 1;
+            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.checkBox7);
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.checkBox6);
+            this.panel2.Controls.Add(this.checkBox5);
+            this.panel2.Controls.Add(this.checkBox4);
+            this.panel2.Controls.Add(this.refreshOrderButton);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.numericUpDown1);
@@ -391,53 +419,30 @@
             this.panel2.Size = new System.Drawing.Size(1000, 171);
             this.panel2.TabIndex = 0;
             // 
-            // splitter2
+            // button1
             // 
-            this.splitter2.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.splitter2.Location = new System.Drawing.Point(0, 0);
-            this.splitter2.Name = "splitter2";
-            this.splitter2.Size = new System.Drawing.Size(204, 171);
-            this.splitter2.TabIndex = 0;
-            this.splitter2.TabStop = false;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(783, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Make the order";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // comboBox1
+            // label2
             // 
-            this.comboBox1.DisplayMember = "Text";
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(334, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(173, 29);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.ValueMember = "Value";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(330, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 21);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Choose cookie";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(215, 10);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(589, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Amount (kg)";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(334, 96);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(593, 108);
             this.numericUpDown1.Minimum = new decimal(new int[] {
             1,
             0,
@@ -452,39 +457,116 @@
             0,
             0});
             // 
-            // label2
+            // pictureBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(330, 72);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 21);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Amount (kg)";
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(448, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // label1
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(590, 96);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(186, 43);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Make the order.";
-            this.button1.UseVisualStyleBackColor = false;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(589, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose cookie";
             // 
-            // dataGridView1
+            // comboBox1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 171);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1000, 390);
-            this.dataGridView1.TabIndex = 1;
+            this.comboBox1.DisplayMember = "Text";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(593, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 29);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.ValueMember = "Value";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // splitter2
+            // 
+            this.splitter2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.splitter2.Location = new System.Drawing.Point(0, 0);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(419, 171);
+            this.splitter2.TabIndex = 0;
+            this.splitter2.TabStop = false;
+            // 
+            // refreshOrderButton
+            // 
+            this.refreshOrderButton.Location = new System.Drawing.Point(289, 113);
+            this.refreshOrderButton.Name = "refreshOrderButton";
+            this.refreshOrderButton.Size = new System.Drawing.Size(110, 45);
+            this.refreshOrderButton.TabIndex = 7;
+            this.refreshOrderButton.Text = "Refresh";
+            this.refreshOrderButton.UseVisualStyleBackColor = true;
+            this.refreshOrderButton.Click += new System.EventHandler(this.RefreshOrderButton_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.checkBox4.Location = new System.Drawing.Point(22, 93);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(91, 25);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "Cookie";
+            this.checkBox4.UseVisualStyleBackColor = false;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.checkBox5.Location = new System.Drawing.Point(22, 133);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(98, 25);
+            this.checkBox5.TabIndex = 9;
+            this.checkBox5.Text = "Amount";
+            this.checkBox5.UseVisualStyleBackColor = false;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.checkBox6.Location = new System.Drawing.Point(22, 13);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(127, 25);
+            this.checkBox6.TabIndex = 10;
+            this.checkBox6.Text = "Order date";
+            this.checkBox6.UseVisualStyleBackColor = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(162, 10);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(129, 28);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DisplayMember = "Text";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(162, 49);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(246, 29);
+            this.comboBox2.TabIndex = 12;
+            this.comboBox2.ValueMember = "Value";
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.checkBox7.Location = new System.Drawing.Point(22, 53);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(110, 25);
+            this.checkBox7.TabIndex = 13;
+            this.checkBox7.Text = "Manager";
+            this.checkBox7.UseVisualStyleBackColor = false;
             // 
             // StoreManagerForm
             // 
@@ -511,11 +593,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.storeVisitEmployeeDataGridView)).EndInit();
             this.workVisitPanel.ResumeLayout(false);
             this.orderPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -562,5 +644,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button refreshOrderButton;
     }
 }
--- Factory_And_store/StoreManagerForm.cs
+++ Factory_And_store/StoreManagerForm.cs
@@ -25,6 +25,7 @@ namespace Factory_And_store
 
             dataGridView1.Columns.Add("OrderNum", "№");
             dataGridView1.Columns.Add("OrderCookieName", "Cookie name");
+            dataGridView1.Columns.Add("Store", "Store");
             dataGridView1.Columns.Add("OrderCookieAmount", "Weight");
             dataGridView1.Columns.Add("OrderDate", "Date");
             dataGridView1.Columns.Add("Manager", "Manager");
@@ -183,20 +184,17 @@ namespace Factory_And_store
                 comboBox1.Items.Add(new { Text = $"{i.ProductName}", Value = i });
             }
 
-            int c = 0;
-            foreach (var i in tempOrders)
-            {
-                c++;
-                var tempManagers = GlobalHelper.Store.Employees.Where(item => item.ID_Employee == i.ID_StoreManager);
-                dataGridView1.Rows.Add(c.ToString(),
-                    $"{tempCookies.Where(item => item.ID_Product == i.ID_Product).Select(item => item.ProductName).Single().ToString()}",
-                    $"{i.Weight}",
-                    $"{i.InitialDate}",
-                    $"{tempManagers.ToString()}");
-            }
+            refreshOrderButton.PerformClick();
             comboBox1.Sorted = true;
             comboBox1.SelectedIndex = 0;
-            
+            string pos = "StoreManager";
+            var comList = GlobalHelper.Store.Employees.Where(item => item.Position.NamePosition == pos);
+            foreach (var i in comList)
+            {
+                comboBox2.Items.Add(new { Text = $"{i.FirstName} {i.SecondName} {i.MiddleName}", Value = i });
+            }
+            comboBox2.Sorted = true;
+            comboBox2.SelectedIndex = 0;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -204,7 +202,65 @@ namespace Factory_And_store
             Guid cookie = (comboBox1.SelectedItem as dynamic).Value.ID_Product;
             var tempCookies = GlobalHelper.Store.Products.Where(item => item.ID_Product == cookie);
             pictureBox1.Image = Image.FromFile(tempCookies.Select(item => item.Photo).Single().ToString());
-            //label2.Text = tempCookies.Select(item => item.Photo).Single().ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Guid produtID = (comboBox1.SelectedItem as dynamic).Value.ID_Product;
+
+            var newOrder = new StoreOrder()
+            {
+                ID_StoreOrder = Guid.NewGuid(),
+                ID_Product = produtID,
+                ID_RealEstateStore = GlobalHelper.User.Employee.RealEstate.ID_RealEstate,
+                ID_StoreManager = GlobalHelper.User.ID_Employee,
+                ID_StatusOrder = GlobalHelper.Store.StatusOrders.Where(item => item.NameStatusOrder == "Created").Select(item => item.ID_StatusOrder).Single(),
+                InitialDate = DateTime.Now,
+                Weight = (int)numericUpDown1.Value
+        };
+
+            GlobalHelper.Store.StoreOrders.Add(newOrder);
+
+            GlobalHelper.Store.SaveChanges();
+
+            refreshOrderButton.PerformClick();
+        }
+
+        private void RefreshOrderButton_Click(object sender, EventArgs e)
+        {
+            var comList = GlobalHelper.Store.StoreOrders.Select(item => item);
+            if (checkBox6.Checked)
+            {
+                comList = comList.Where(item => item.InitialDate == new DateTime(dateTimePicker1.Value.Year, dateTimePicker1.Value.Month, dateTimePicker1.Value.Day));
+            }
+            if (checkBox7.Checked)
+            {
+                Guid workerID = (comboBox2.SelectedItem as dynamic).Value.ID_Employee;
+                comList = comList.Where(item => item.ID_StoreManager == workerID);
+            }
+            if (checkBox4.Checked)
+            {
+                Guid cookieID = (comboBox1.SelectedItem as dynamic).Value.ID_Product;
+                comList = comList.Where(item => item.ID_Product == cookieID);
+            }
+            if (checkBox5.Checked)
+            {
+                var weight = Convert.ToInt32(numericUpDown1.Value);
+                comList = comList.Where(item => item.Weight == weight);
+            }
+            dataGridView1.Rows.Clear();
+            int c = 0;
+            foreach (var i in comList)
+            {
+                c++;
+                dataGridView1.Rows.Add(c.ToString(),
+                    $"{i.Product.ProductName}",
+                    $"{i.RealEstate.NameRealEstate}",
+                    $"{i.Weight}",
+                    $"{i.InitialDate}",
+                    $"{i.Employee.FirstName} {i.Employee.SecondName} {i.Employee.MiddleName}");
+
+            }
         }
     }
 }
